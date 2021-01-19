914b92430a446975d36d5653a39cb2df6d1c61ec
jmball <jmball@users.noreply.github.com>
2021-01-19 11:12:10 +0000

    moved to single heatsink cut

--- cooling_block.py
+++ cooling_block.py
@@ -2,7 +2,7 @@
 
 All dimensions are in mm.
 """
-
+import math
 import pathlib
 
 import cadquery as cq
@@ -12,83 +12,122 @@ build_dir = pathlib.Path("build")
 if not build_dir.is_dir():
     build_dir.mkdir()
 
+# heat sink parameters
+base_h = 10
+fin_h = 15
+
 # block parameters
 block_l = 350
 block_w = 350
+block_h = base_h + fin_h
+
+# led array parameters
+led_array_cols = 11
+led_array_rows = led_array_cols
+led_repeat_footprint_l = block_l / led_array_cols
+led_repeat_footprint_w = block_w / led_array_rows
 
 # o-ring parameters
-oring_edge_gap = 2
+oring_edge_gap = 3
 oring_cs = 2.62
 oring_groove_h = 0.077 * 25.4
 oring_groove_w = 0.1225 * 25.4
 
-# extrusion
+# extrusion (strut) width
 extrusion_w = 20
+bracket_l = 20
+bracket_w = 20
 
 # screw holes for fixing led array/heatsink assembly to support extrusion
 extrusion_screw_clearance_r = 5.5 / 2
+extrusion_screw_tap_r = 4.2 / 2
 extrusion_screw_min_gap = 2
+
+# passthrough holes for wires
+pt_hole_clearance_r = 2.65
+pt_hole_footprint_offset = 4
+pt_hole_x_centers_along_top = []
+for x in range(0, led_array_cols):
+    pt_hole_x_centers_along_top.append(
+        (
+            -block_l / 2 + x * led_repeat_footprint_l + pt_hole_footprint_offset,
+            block_w / 2 - pt_hole_footprint_offset,
+        )
+    )
+pt_hole_x_centers_along_bottom = []
+for x in range(1, led_array_cols + 1):
+    pt_hole_x_centers_along_bottom.append(
+        (
+            -block_l / 2 + x * led_repeat_footprint_l - pt_hole_footprint_offset,
+            -block_w / 2 + pt_hole_footprint_offset,
+        )
+    )
+pt_hole_centers = pt_hole_x_centers_along_top + pt_hole_x_centers_along_bottom
 
 # countersink screws for fixing lid to block
 cs_screw_clearance_r = extrusion_screw_clearance_r
 cs_screw_tap_r = 4.2 / 2
-cs_screw_tread_h = 10
+cs_screw_tread_h = fin_h
 cs_screw_cap_r = 9.2 / 2
 cs_screw_cap_h = 2.5
 cs_screw_angle = 90
 cs_screw_min_gap = extrusion_screw_min_gap
-cs_number = 7
-cs_delta = (block_l - extrusion_w) / (cs_number - 1)
-cs_centers_along_axis = [
-    -(block_l - extrusion_w) / 2 + x * cs_delta for x in range(cs_number)
-]
-cs_holes1 = [(0, x) for x in cs_centers_along_axis]
-cs_holes2 = [((block_l - extrusion_w) / 2, x) for x in cs_centers_along_axis]
-cs_holes3 = [(-(block_l - extrusion_w) / 2, x) for x in cs_centers_along_axis]
-cs_holes4 = [(x, 0) for x in cs_centers_along_axis]
-cs_holes5 = [(x, (block_w - extrusion_w) / 2) for x in cs_centers_along_axis]
-cs_holes6 = [(x, -(block_w - extrusion_w) / 2) for x in cs_centers_along_axis]
-cs_holes = set(cs_holes1 + cs_holes2 + cs_holes3 + cs_holes4 + cs_holes5 + cs_holes6)
+cs_delta_l = 2 * led_repeat_footprint_l
+cs_delta_w = 2 * led_repeat_footprint_w
+cs_number_l = math.floor(led_array_cols / 2)
+cs_number_w = math.floor(led_array_rows / 2)
 
-# extrusion holes
-extrusion_screw_number = cs_number - 1
-extrusion_screw_delta = (block_l - extrusion_w - cs_delta) / (
-    extrusion_screw_number - 1
-)
-extrusion_screw_centers_along_axis = [
-    -(block_l - extrusion_w - cs_delta) / 2 + x * extrusion_screw_delta
-    for x in range(extrusion_screw_number)
+cs_centers_along_x_axis_1 = [
+    -block_l / 2
+    + pt_hole_footprint_offset
+    + x * cs_delta_l
+    + 3 * led_repeat_footprint_l / 2
+    for x in range(cs_number_l)
 ]
-extrusion_holes1 = [
-    (x, (block_w - extrusion_w) / 2) for x in extrusion_screw_centers_along_axis
+cs_centers_along_x_axis_2 = [
+    block_l / 2
+    - pt_hole_footprint_offset
+    - x * cs_delta_l
+    - 3 * led_repeat_footprint_l / 2
+    for x in range(cs_number_l)
 ]
-extrusion_holes2 = [
-    (x, -(block_l - extrusion_w) / 2) for x in extrusion_screw_centers_along_axis
+cs_centers_along_y_axis_1 = [
+    -block_w / 2
+    + pt_hole_footprint_offset
+    + y * cs_delta_w
+    + 3 * led_repeat_footprint_w / 2
+    for y in range(cs_number_w)
 ]
-extrusion_holes = set(extrusion_holes1 + extrusion_holes2)
+cs_centers_along_y_axis_2 = [
+    block_w / 2
+    - pt_hole_footprint_offset
+    - y * cs_delta_w
+    - 3 * led_repeat_footprint_w / 2
+    for y in range(cs_number_w)
+]
 
-# heat sink parameters
-base_h = 10
-fin_h = 15
-heatsink_cut_l = (
-    (
-        block_l
-        - extrusion_w
-        - 2
-        * (
-            extrusion_screw_clearance_r
-            + extrusion_screw_min_gap
-            + cs_screw_clearance_r
-            + cs_screw_min_gap
-            + 2 * oring_edge_gap
-        )
-    )
-    / 2
-    - 2 * oring_groove_w
-    - 2 * oring_edge_gap
-)
+cs_holes1 = [(x, (block_w - extrusion_w) / 2) for x in cs_centers_along_x_axis_1]
+cs_holes2 = [(x, -(block_w - extrusion_w) / 2) for x in cs_centers_along_x_axis_2]
+
+cs_holes3 = [(-(block_l - extrusion_w) / 2, y) for y in cs_centers_along_y_axis_1]
+cs_holes4 = [((block_l - extrusion_w) / 2, y) for y in cs_centers_along_y_axis_2]
+
+cs_holes = cs_holes1 + cs_holes2 + cs_holes3 + cs_holes4
+
+# extrusion (strut) mounting holes
+extrusion_screw_tread_h = fin_h
+
+extrusion_holes = [
+    (-(block_l - extrusion_w) / 2, -block_w / 2 + extrusion_w + bracket_w / 2),
+    (-(block_l - extrusion_w) / 2, block_w / 2 - extrusion_w - bracket_w / 2),
+    ((block_l - extrusion_w) / 2, -block_w / 2 + extrusion_w + bracket_w / 2),
+    ((block_l - extrusion_w) / 2, block_w / 2 - extrusion_w - bracket_w / 2),
+]
+
+# more heat sink parameters
+heatsink_cut_l = block_l - 2 * (extrusion_w + oring_edge_gap + oring_groove_w)
 heatsink_cut_w = heatsink_cut_l
-fin_gap = 6
+fin_gap = 10
 approx_fin_t = 2
 fin_number = int((heatsink_cut_w - fin_gap) / (approx_fin_t + fin_gap))
 if fin_number % 2 == 0:
@@ -122,14 +161,34 @@ lid_h = 10
 # screw holes fixing led array to block
 led_screw_tap_r = 2.5 / 2
 led_screw_tap_h = 5
-led_array_rows_cols = 12
-led_repeat_footprint_l = (block_l - 2 * extrusion_w) / led_array_rows_cols
-led_screw_centers_along_axis = [
-    -block_l / 2 + extrusion_w + x * led_repeat_footprint_l
-    for x in range(led_array_rows_cols + 1)
+
+# regular grid of screws
+led_screw_centers_along_x_axis = [
+    -block_l / 2 + x * led_repeat_footprint_l for x in range(1, led_array_cols)
 ]
+led_screw_centers_along_y_axis = [
+    -block_w / 2 + y * led_repeat_footprint_w + (led_repeat_footprint_w / 2)
+    for y in range(0, led_array_rows)
+]
+
+# irregular positioned screws along left and right sides
+led_screw_side_offset = 3.5
+led_screw_centers_along_left = [
+    (-block_l / 2 + led_screw_side_offset, y) for y in led_screw_centers_along_y_axis
+]
+led_screw_centers_along_right = [
+    (block_l / 2 - led_screw_side_offset, y) for y in led_screw_centers_along_y_axis
+]
+
+# all screw holes
 led_screw_holes = set(
-    [(y, x) for x in led_screw_centers_along_axis for y in led_screw_centers_along_axis]
+    [
+        (x, y)
+        for x in led_screw_centers_along_x_axis
+        for y in led_screw_centers_along_y_axis
+    ]
+    + led_screw_centers_along_left
+    + led_screw_centers_along_right
 )
 
 # screw holes for standoffs for attaching a window/diffuser
@@ -302,62 +361,62 @@ def block(
     # add holes for extrusion screws
     block = block.faces(">Z").workplane(centerOption="CenterOfBoundBox")
     block = block.pushPoints(extrusion_holes)
-    block = block.hole(2 * extrusion_screw_clearance_r)
+    block = block.hole(2 * extrusion_screw_tap_r, depth=extrusion_screw_tread_h)
+
+    # add holes for wire passthroughs
+    block = block.faces(">Z").workplane(centerOption="CenterOfBoundBox")
+    block = block.pushPoints(pt_hole_centers)
+    block = block.hole(2 * pt_hole_clearance_r)
 
     # add holes for led screws
     block = block.faces("<Z").workplane(centerOption="CenterOfBoundBox")
     block = block.pushPoints(led_screw_holes)
     block = block.hole(2 * led_screw_tap_r, depth=led_screw_tap_h)
 
-    # add holes for standoffs
-    block = block.faces("<Z").workplane(centerOption="CenterOfBoundBox")
-    block = block.pushPoints(standoff_screw_holes)
-    block = block.hole(2 * standoff_screw_tap_r, depth=standoff_screw_tap_h)
+    # # add holes for standoffs
+    # block = block.faces("<Z").workplane(centerOption="CenterOfBoundBox")
+    # block = block.pushPoints(standoff_screw_holes)
+    # block = block.hole(2 * standoff_screw_tap_r, depth=standoff_screw_tap_h)
 
     # create o-ring and heatsink cutouts
     oring_groove = oring_groove.translate((0, 0, (fin_h - oring_groove_h) / 2))
 
     heatsink_oring1 = oring_groove.union(heatsink_cutout)
-    heatsink_oring1 = heatsink_oring1.translate(
-        (
-            block_l / 2 - heatsink_offset,
-            block_w / 2 - heatsink_offset,
-            (block_h - fin_h) / 2,
-        )
-    )
+    heatsink_oring1 = heatsink_oring1.translate((0, 0, (block_h - fin_h) / 2,))
+    heatsink_oring1 = heatsink_oring1.rotate((0, 0, 0), (0, 0, 1), 90)
 
-    heatsink_oring2 = oring_groove.union(heatsink_cutout)
-    heatsink_oring2 = heatsink_oring2.translate(
-        (
-            -block_l / 2 + heatsink_offset,
-            block_w / 2 - heatsink_offset,
-            (block_h - fin_h) / 2,
-        )
-    )
+    # heatsink_oring2 = oring_groove.union(heatsink_cutout)
+    # heatsink_oring2 = heatsink_oring2.translate(
+    #     (
+    #         -block_l / 2 + heatsink_offset,
+    #         block_w / 2 - heatsink_offset,
+    #         (block_h - fin_h) / 2,
+    #     )
+    # )
 
-    heatsink_oring3 = oring_groove.union(heatsink_cutout)
-    heatsink_oring3 = heatsink_oring3.translate(
-        (
-            block_l / 2 - heatsink_offset,
-            -block_w / 2 + heatsink_offset,
-            (block_h - fin_h) / 2,
-        )
-    )
+    # heatsink_oring3 = oring_groove.union(heatsink_cutout)
+    # heatsink_oring3 = heatsink_oring3.translate(
+    #     (
+    #         block_l / 2 - heatsink_offset,
+    #         -block_w / 2 + heatsink_offset,
+    #         (block_h - fin_h) / 2,
+    #     )
+    # )
 
-    heatsink_oring4 = oring_groove.union(heatsink_cutout)
-    heatsink_oring4 = heatsink_oring4.translate(
-        (
-            -block_l / 2 + heatsink_offset,
-            -block_w / 2 + heatsink_offset,
-            (block_h - fin_h) / 2,
-        )
-    )
+    # heatsink_oring4 = oring_groove.union(heatsink_cutout)
+    # heatsink_oring4 = heatsink_oring4.translate(
+    #     (
+    #         -block_l / 2 + heatsink_offset,
+    #         -block_w / 2 + heatsink_offset,
+    #         (block_h - fin_h) / 2,
+    #     )
+    # )
 
     # cut heatsink and oring grooves
     block = block.cut(heatsink_oring1)
-    block = block.cut(heatsink_oring2)
-    block = block.cut(heatsink_oring3)
-    block = block.cut(heatsink_oring4)
+    # block = block.cut(heatsink_oring2)
+    # block = block.cut(heatsink_oring3)
+    # block = block.cut(heatsink_oring4)
 
     return block
 
@@ -378,6 +437,11 @@ def lid():
     lid = lid.faces(">Z").workplane(centerOption="CenterOfBoundBox")
     lid = lid.pushPoints(extrusion_holes)
     lid = lid.hole(2 * extrusion_screw_clearance_r)
+
+    # add holes for wire passthroughs
+    lid = lid.faces(">Z").workplane(centerOption="CenterOfBoundBox")
+    lid = lid.pushPoints(pt_hole_centers)
+    lid = lid.hole(2 * pt_hole_clearance_r)
 
     return lid
 
